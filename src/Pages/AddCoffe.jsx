a931f0d9b294001c66142277ec490818a13f5e32
Maruf Hasan Shihab <devmarufhasan@gmail.com>
2024-12-08 21:40:37 +0600

    get add coffe all data from addCoffe form

--- src/Pages/AddCoffe.jsx
+++ src/Pages/AddCoffe.jsx
@@ -1,12 +1,25 @@
 import BackButton from "./../Components/BackButton";
 
 const AddCoffe = () => {
+    const handleAddCoffe = e=>{
+        e.preventDefault();
+        const form = e.target;
+        const name = form.name.value.trim();
+        const chef = form.chef.value.trim();
+        const supplier = form.supplier.value.trim();
+        const taste = form.taste.value.trim();
+        const category = form.category.value.trim();
+        const details = form.details.value.trim();
+        const photo = form.photo.value.trim();
+        const coffeFormData = {name,chef,supplier,taste,category,details,photo}
+        console.log(coffeFormData)
+    }
   return (
     <div>
       <div className="py-4 px-12">
         <BackButton />
       </div>
-      <div className="flex justify-center p-6 mx-16 mb-16 bg-gray-200 shadow-md rounded">
+      <div className="flex justify-center p-6 mx-16 mb-16 bg-[#f4f3f0] shadow-md rounded">
         <div>
           <h2 className="text-gray-700 text-[45px] text-center font-normal font-rancho">
             Add New Coffee
@@ -17,7 +30,7 @@ const AddCoffe = () => {
             of using Lorem Ipsum is that it has a more-or-less normal
             distribution of letters, as opposed to using Content here.
           </p>
-          <form className="py-8">
+          <form onSubmit={handleAddCoffe} className="py-8">
             <div className="grid sm:grid-cols-2 gap-8">
               <div>
                 <label className="text-[#1a1919]/80 text-xl font-semibold font-relway leading-[30px]">
@@ -93,7 +106,7 @@ const AddCoffe = () => {
               <input
                 name="photo"
                 type="text"
-                className="bg-gray-100 w-full text-gray-800 text-sm px-4 py-3.5 rounded-md focus:bg-transparent outline-blue-500 transition-all w-full"
+                className="bg-gray-100 w-full text-gray-800 text-sm px-4 py-3.5 rounded-md focus:bg-transparent outline-blue-500 transition-all "
                 placeholder="Enter photo URL"
               />
             </div>
