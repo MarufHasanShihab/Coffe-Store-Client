8b686edcc1e966aed0ce1be0817b8c004aa46b02
Maruf Hasan Shihab <devmarufhasan@gmail.com>
2024-12-11 21:07:46 +0600

    added condition if coffes.length >1 display all coffe else display coffe not found text

--- src/Components/Home/Products.jsx
+++ src/Components/Home/Products.jsx
@@ -43,44 +43,52 @@ const Products = ({ loadedCoffe }) => {
     <div className="p-24">
       <Title subTitle="--- Sip & Savor ---" title="Our Popular Products" />
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6 ">
-        {coffes?.map((coffe) => (
-          <div
-            key={coffe._id}
-            className="grid grid-cols-1 items-center md:grid-cols-3 gap-4 bg-[#F5F4F1] shadow-md rounded-[8px] p-5">
-            <img src={coffe.photo} alt="coffe_img" />
-            <div className="space-y-4">
-              <h4 className="text-[#1b1a1a] text-[18px] font-semibold font-relway">
-                Name :{" "}
-                <span className="text-[#5c5b5b] text-[16px] font-normal">
-                  {coffe.name}
-                </span>
-              </h4>
-              <h4 className="text-[#1b1a1a] text-[18px] font-semibold font-relway">
-                Chef :{" "}
-                <span className="text-[#5c5b5b] text-[16px] font-normal">
-                  {coffe.chef}
-                </span>
-              </h4>
-              <h4 className="text-[#1b1a1a] text-[18px] font-semibold font-relway">
-                Price :{" "}
-                <span className="text-[#5c5b5b] text-[16px] font-normal">
-                  {coffe.price} Taka
-                </span>
-              </h4>
-            </div>
-            <div className="flex md:flex-col gap-10 md:gap-6 text-white">
-              <Link to={`/coffe-details/${coffe._id}`} className="p-2 w-[30px] bg-[#D2B48C] rounded-[5px] shadow-sm">
-                <FaEye />
-              </Link>
-              <Link to={`/update-coffe/${coffe._id}`} className="p-2 w-[30px] bg-[#3C393B] rounded-[5px] shadow-sm">
-                <MdEdit />
-              </Link>
-              <button onClick={()=>handleDeleteCoffe(coffe._id)} className="p-2 w-[30px] bg-[#EA4744] rounded-[5px] shadow-sm">
-                <MdDelete />
-              </button>
-            </div>
-          </div>
-        ))}
+        {
+          coffes.length >0 ? (
+            coffes?.map((coffe) => (
+              <div
+                key={coffe._id}
+                className="grid grid-cols-1 items-center md:grid-cols-3 gap-4 bg-[#F5F4F1] shadow-md rounded-[8px] p-5">
+                <img src={coffe.photo} alt="coffe_img" />
+                <div className="space-y-4">
+                  <h4 className="text-[#1b1a1a] text-[18px] font-semibold font-relway">
+                    Name :{" "}
+                    <span className="text-[#5c5b5b] text-[16px] font-normal">
+                      {coffe.name}
+                    </span>
+                  </h4>
+                  <h4 className="text-[#1b1a1a] text-[18px] font-semibold font-relway">
+                    Chef :{" "}
+                    <span className="text-[#5c5b5b] text-[16px] font-normal">
+                      {coffe.chef}
+                    </span>
+                  </h4>
+                  <h4 className="text-[#1b1a1a] text-[18px] font-semibold font-relway">
+                    Price :{" "}
+                    <span className="text-[#5c5b5b] text-[16px] font-normal">
+                      {coffe.price} Taka
+                    </span>
+                  </h4>
+                </div>
+                <div className="flex md:flex-col gap-10 md:gap-6 text-white">
+                  <Link to={`/coffe-details/${coffe._id}`} className="p-2 w-[30px] bg-[#D2B48C] rounded-[5px] shadow-sm">
+                    <FaEye />
+                  </Link>
+                  <Link to={`/update-coffe/${coffe._id}`} className="p-2 w-[30px] bg-[#3C393B] rounded-[5px] shadow-sm">
+                    <MdEdit />
+                  </Link>
+                  <button onClick={()=>handleDeleteCoffe(coffe._id)} className="p-2 w-[30px] bg-[#EA4744] rounded-[5px] shadow-sm">
+                    <MdDelete />
+                  </button>
+                </div>
+              </div>
+            ))
+          ) : ( <div className="w-ful h-[70vh] flex items-center justify-center">
+            <h2 className="text-2xl font-bold flex items-center">
+              Coffe Not Found
+            </h2>
+          </div>)
+        }
       </div>
     </div>
   );
--- src/Pages/Users.jsx
+++ src/Pages/Users.jsx
@@ -4,80 +4,86 @@ import Swal from "sweetalert2";
 
 const Users = () => {
   const loadedUsers = useLoaderData();
-  const [users,setUsers] = useState(loadedUsers);
-  const handleDeleteUser = id =>{
+  const [users, setUsers] = useState(loadedUsers);
+  const handleDeleteUser = (id) => {
     Swal.fire({
-        title: "Are you sure?",
-        text: "You won't be able to revert this!",
-        icon: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#3085d6",
-        cancelButtonColor: "#d33",
-        confirmButtonText: "Yes, delete it!"
-      }).then((result) => {
-        if (result.isConfirmed) {
-            fetch(`http://localhost:5000/users/${id}`,{
-                method: "DELETE"
-            })
-            .then(res => res.json())
-            .then(data =>{
-                if(data.deletedCount === 1){
-                    const reamaning = users.filter(user => user._id !== id);
-                    setUsers(reamaning);
-                    Swal.fire({
-                        title: "Deleted!",
-                        text: "Your file has been deleted.",
-                        icon: "success"
-                      });
-                }
-            })
-        }
-      });
-  }
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/users/${id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount === 1) {
+              const reamaning = users.filter((user) => user._id !== id);
+              setUsers(reamaning);
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your file has been deleted.",
+                icon: "success",
+              });
+            }
+          });
+      }
+    });
+  };
   return (
     <div className="w-full h-screen p-5 font-relway">
       <div>
-        {
-          users.length >0 ? (
-            <table className="min-w-full divide-y divide-gray-200">
-          <thead>
-            <tr>
-              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Id
-              </th>
-              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Email
-              </th>
-              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Created At
-              </th>
-              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Action
-              </th>
-            </tr>
-          </thead>
-          <tbody className="bg-white divide-y divide-gray-200">
-            {
-              users?.map((user) => (
+        {users.length > 0 ? (
+          <table className="min-w-full divide-y divide-gray-200">
+            <thead>
+              <tr>
+                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Id
+                </th>
+                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Email
+                </th>
+                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Created At
+                </th>
+                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Action
+                </th>
+              </tr>
+            </thead>
+            <tbody className="bg-white divide-y divide-gray-200">
+              {users?.map((user) => (
                 <tr key={user._id}>
                   <td className="px-6 py-4 whitespace-nowrap">{user._id}</td>
                   <td className="px-6 py-4 whitespace-nowrap">{user.email}</td>
-                  <td className="px-6 py-4 whitespace-nowrap">{user.creatAt}</td>
+                  <td className="px-6 py-4 whitespace-nowrap">
+                    {user.creatAt}
+                  </td>
                   <td className="px-6 py-4 whitespace-nowrap">
                     <button className="px-4 py-2 font-medium text-white bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:shadow-outline-blue active:bg-blue-600 transition duration-150 ease-in-out">
                       Edit
                     </button>
-                    <button onClick={()=>handleDeleteUser(user._id)} className="ml-2 px-4 py-2 font-medium text-white bg-red-600 rounded-md hover:bg-red-500 focus:outline-none focus:shadow-outline-red active:bg-red-600 transition duration-150 ease-in-out">
+                    <button
+                      onClick={() => handleDeleteUser(user._id)}
+                      className="ml-2 px-4 py-2 font-medium text-white bg-red-600 rounded-md hover:bg-red-500 focus:outline-none focus:shadow-outline-red active:bg-red-600 transition duration-150 ease-in-out">
                       Delete
                     </button>
                   </td>
                 </tr>
-              ))
-            }
-          </tbody>
-        </table>
-          ): (<div className="w-ful h-[70vh] flex items-center justify-center"><h2 className="text-2xl font-bold flex items-center">Users Not Found</h2></div>)
-        }
+              ))}
+            </tbody>
+          </table>
+        ) : (
+          <div className="w-ful h-[70vh] flex items-center justify-center">
+            <h2 className="text-2xl font-bold flex items-center">
+              Users Not Found
+            </h2>
+          </div>
+        )}
       </div>
     </div>
   );
